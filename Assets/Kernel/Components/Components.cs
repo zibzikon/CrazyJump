bcfc02ecfcb07780d6291082ed8988743b2f9ca3
zibzikon <thezibzikon@gmail.com>
2023-03-30 17:48:09 +0300

    Fix: Added IComponent implementation for all components

--- Assets/Kernel/Components/Components.cs
+++ Assets/Kernel/Components/Components.cs
@@ -1,10 +1,11 @@
+using Entitas;
 using Entitas.CodeGeneration.Attributes;
 using UnityEngine;
 using static Entitas.CodeGeneration.Attributes.EventTarget;
 // @formatter:off
 
-[Game] public class PlayerCharacter { }
-[Game] public class Movable { }
-[Game] public class MovePosition { private Vector3 Value; }
-[Game, Event(Self)] public class Position { private Vector3 Value; }
-[Game, Event(Self)] public class Rotation { private Quaternion Value; }
+[Game] public class PlayerCharacter : IComponent { }
+[Game] public class Movable : IComponent { }
+[Game] public class MovePosition : IComponent { private Vector3 Value; }
+[Game, Event(Self)] public class Position : IComponent { private Vector3 Value; }
+[Game, Event(Self)] public class Rotation : IComponent { private Quaternion Value; }
