9344ac69ec66122a04d26f1e1a2669da6d8a74a9
zibzikon <thezibzikon@gmail.com>
2023-03-30 17:47:01 +0300

    Update: Added main components into Components.cs

--- Assets/Kernel/Components/Components.cs
+++ Assets/Kernel/Components/Components.cs
@@ -1,1 +1,10 @@
+using Entitas.CodeGeneration.Attributes;
+using UnityEngine;
+using static Entitas.CodeGeneration.Attributes.EventTarget;
+// @formatter:off
 
+[Game] public class PlayerCharacter { }
+[Game] public class Movable { }
+[Game] public class MovePosition { private Vector3 Value; }
+[Game, Event(Self)] public class Position { private Vector3 Value; }
+[Game, Event(Self)] public class Rotation { private Quaternion Value; }
---
+++ Assets/Kernel/Systems/MovingSystem.cs
@@ -0,0 +1,12 @@
+using Entitas;
+
+namespace Kernel.Systems
+{
+    public class MovingSystem : IExecuteSystem
+    {
+        public void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
